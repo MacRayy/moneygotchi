c1575d24e9a309ee3864c69f5f698cfe3e055d98
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-04-13 22:06:25 +0200

    feat(app.js): create app.js and jsoninput files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import JsonImport from './json-import.js'
+
+const App = function() {
+  console.log('alma')
+
+  JsonImport.log()
+}
+
+App()
---
+++ js/json-import.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const JsonImport = (function() {
+  const log = () => {
+    console.log('json')
+  }
+
+  return {
+    log
+  }
+})()
+
+export default JsonImport
