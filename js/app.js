207b9cfeb505a50c5d1ccd8d8bc6caac93b66379
Hartmann Ferenc <ferenc.hartmann@blackswan.com>
2018-04-13 22:27:13 +0200

    feat(json import): json file import modul create

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@
 import JsonImport from './json-import.js'
 
 const App = function() {
+  const jsonPath = '../json-data/account_10731833-43130014.json'
+  let jsonData;
+
   console.log('alma')
 
-  JsonImport.log()
+  JsonImport.getJsonData(jsonPath, function(response) {
+    jsonData = response
+    console.log(jsonData)
+  })
+
 }
 
 App()
